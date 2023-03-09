1fcd454475d194e6220f4bc941f595b0e99f4f8b
Steve Waugh <stevewaughbitrix@gmail.com>
2023-03-09 12:04:02 +0530

    Configure Tailwind CSS

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <div>
+        <h1>Login</h1>
+      </div>
+    </>
+  );
+}
---
+++ pages/register.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Register() {
+  return (
+    <>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <div>Register</div>
+    </>
+  );
+}
