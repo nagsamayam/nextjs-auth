55a76567587b6be74c81c95ef3166870a4fd7916
Steve Waugh <stevewaughbitrix@gmail.com>
2023-03-09 12:32:46 +0530

    Add layout component

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,17 @@
+import Layout from "@/layout/layout";
 import Head from "next/head";
 
 export default function Login() {
   return (
     <>
-      <Head>
-        <title>Login</title>
-      </Head>
-      <div>
-        <h1>Login</h1>
-      </div>
+      <Layout>
+        <Head>
+          <title>Login</title>
+        </Head>
+        <div>
+          <h1>Login</h1>
+        </div>
+      </Layout>
     </>
   );
 }
