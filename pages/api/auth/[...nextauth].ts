1b32c908ad15981bdcb798b8e142e9f33546a603
Steve Waugh <stevewaughbitrix@gmail.com>
2023-03-09 20:04:52 +0530

    Github authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   providers: [
@@ -7,6 +8,11 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.OAUTH_GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.OAUTH_GOOGLE_CLIENT_SECRET as string,
+    }),
+    // Github Provider
+    GitHubProvider({
+      clientId: process.env.OAUTH_GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.OAUTH_GITHUB_CLIENT_SECRET as string,
     }),
   ],
 });
